89c327a233e3fc11e110f5a0b23e7e0d98eda113
kasemm <mohamed.kasem@gig.tech>
2021-01-30 21:24:33 +0200

    Finish up  edge cases. Add tester script

--- opencl_elections.c
+++ opencl_elections.c
@@ -1,4 +1,3 @@
-
 #include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -8,7 +7,33 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <CL/opencl.h>
+#include <time.h>
 
+////////////////////////////////////////////////////////////////////////////////
+void getTop2(int *votes, int C, int *top2)
+{
+    top2[0] = top2[1] = -1;
+    int max_vote = INT32_MIN;
+    for (int i = 0; i < C; i++)
+    {
+        if (votes[i] > max_vote)
+        {
+            top2[0] = i;
+            max_vote = votes[i];
+        }
+    }
+    max_vote = INT32_MIN;
+    for (int i = 0; i < C; i++)
+    {
+        if (i == top2[0])
+            continue;
+        if (votes[i] > max_vote)
+        {
+            top2[1] = i;
+            max_vote = votes[i];
+        }
+    }
+}
 ////////////////////////////////////////////////////////////////////////////////
 
 void readLine(FILE *fp, int C, int *out)
@@ -26,21 +51,23 @@ void readLine(FILE *fp, int C, int *out)
 int main(int argc, char **argv)
 {
     int err; // error code returned from api calls
-
     int candidates;
     int voters;
+
     FILE *fp1;
-    fp1 = fopen("input.txt", "r");                                   // File to read from, should be accessible to all processes
+    fp1 = fopen("input.txt", "r"); // File to read from, should be accessible to all processes
     if (!fp1)
     {
         printf("Couldn't open input.txt\n");
         return 1;
     }
-    fscanf(fp1, "%d\n", &candidates);                                     // Read number of Candidates
-    fscanf(fp1, "%d\n", &voters);                                         // Read number of Voters
+    fscanf(fp1, "%d\n", &candidates); // Read number of Candidates
+    fscanf(fp1, "%d\n", &voters);     // Read number of Voters
+
     int *h_vote_pref = (int *)malloc(sizeof(int) * candidates * voters); // original data set given to device
     int *results = (int *)malloc(sizeof(int) * candidates);              // original data set given to device
-    unsigned int correct;                                                // number of correct results returned
+    int winner = 0;
+    double *percent_votes = (double *)malloc(candidates * sizeof(double)); // Array to hold total votes
 
     for (int i = 0; i < voters; i++)
     {
@@ -54,11 +81,18 @@ int main(int argc, char **argv)
     cl_context context;        // compute context
     cl_command_queue commands; // compute command queue
     cl_program program;        // compute program
-    cl_kernel kernel;          // compute kernel
+    cl_kernel round1_kernel;   // compute round1_kernel
+    cl_kernel round2_kernel;   // compute round1_kernel
 
-    cl_mem d_vote_pref; // device memory used for the input array
-    cl_mem output;      // device memory used for the output array
+    cl_mem d_vote_pref;
 
+    // round 1 buffers
+    cl_mem d_local_vote_sum;
+    cl_mem output;
+
+    // round 2 buffers
+    cl_mem d_local_top2_sum;
+    cl_mem d_top2_sum;
     // Fill our data set with random float values
     //
     char *KernelSource;
@@ -68,7 +102,7 @@ int main(int argc, char **argv)
     fp = fopen("red_kernel.cl", "rb");
     if (!fp)
     {
-        printf("Failed to load kernel\n");
+        printf("Failed to load kernels\n");
         return 1;
     }
 
@@ -132,18 +166,22 @@ int main(int argc, char **argv)
 
     // Create the compute kernel in the program we wish to run
     //
-    kernel = clCreateKernel(program, "vote_sum", &err);
-    if (!kernel || err != CL_SUCCESS)
+    round1_kernel = clCreateKernel(program, "round1_k", &err);
+    if (!round1_kernel || err != CL_SUCCESS)
     {
-        printf("Error: Failed to create compute kernel!\n");
+        printf("Error: Failed to create compute round1_kernel!\n");
         exit(1);
     }
 
     // Create the input and output arrays in device memory for our calculation
     //
     d_vote_pref = clCreateBuffer(context, CL_MEM_READ_ONLY, sizeof(int) * voters * candidates, NULL, NULL);
+    d_local_vote_sum = clCreateBuffer(context, CL_MEM_READ_WRITE, sizeof(int) * candidates, NULL, NULL);
     output = clCreateBuffer(context, CL_MEM_WRITE_ONLY, sizeof(int) * candidates, NULL, NULL);
-    if (!d_vote_pref || !output)
+
+    d_local_top2_sum = clCreateBuffer(context, CL_MEM_READ_WRITE, sizeof(int) * 2, NULL, NULL);
+    d_top2_sum = clCreateBuffer(context, CL_MEM_WRITE_ONLY, sizeof(int) * 2, NULL, NULL);
+    if (!d_vote_pref || !output || !d_local_vote_sum || !d_local_top2_sum || !d_top2_sum)
     {
         printf("Error: Failed to allocate device memory!\n");
         exit(1);
@@ -158,36 +196,41 @@ int main(int argc, char **argv)
         exit(1);
     }
 
-    // Set the arguments to our compute kernel
+    // Set the arguments to our compute round1_kernel
     //
     err = 0;
-    err = clSetKernelArg(kernel, 0, sizeof(cl_mem), &d_vote_pref);
-    err |= clSetKernelArg(kernel, 1, sizeof(cl_mem), &output);
-    err |= clSetKernelArg(kernel, 2, sizeof(unsigned int), &candidates);
-    err |= clSetKernelArg(kernel, 3, sizeof(unsigned int), &voters);
+    err = clSetKernelArg(round1_kernel, 0, sizeof(cl_mem), &d_vote_pref);
+    err |= clSetKernelArg(round1_kernel, 1, sizeof(cl_mem), &output);
+    err |= clSetKernelArg(round1_kernel, 2, sizeof(cl_mem), NULL);
+    err |= clSetKernelArg(round1_kernel, 3, sizeof(int), &candidates);
+    err |= clSetKernelArg(round1_kernel, 4, sizeof(int), &voters);
     if (err != CL_SUCCESS)
     {
-        printf("Error: Failed to set kernel arguments! %d\n", err);
+        printf("Error: Failed to set round1_kernel arguments! %d\n", err);
         exit(1);
     }
 
-    // Get the maximum work group size for executing the kernel on the device
+    // Get the maximum work group size for executing the round1_kernel on the device
     //
-    err = clGetKernelWorkGroupInfo(kernel, device_id, CL_KERNEL_WORK_GROUP_SIZE, sizeof(local), &local, NULL);
+    err = clGetKernelWorkGroupInfo(round1_kernel, device_id, CL_KERNEL_WORK_GROUP_SIZE, sizeof(local), &local, NULL);
     if (err != CL_SUCCESS)
     {
-        printf("Error: Failed to retrieve kernel work group info! %d\n", err);
+        printf("Error: Failed to retrieve round1_kernel work group info! %d\n", err);
         exit(1);
     }
 
-    // Execute the kernel over the entire range of our 1d input data set
+    // Execute the round1_kernel over the entire range of our 1d input data set
     // using the maximum number of work group items for this device
     //
+    clock_t start, end;
+    double cpu_time_used;
+
+    start = clock();
     global = voters * candidates < local ? local : ceil(voters * candidates / (float)local) * local;
-    err = clEnqueueNDRangeKernel(commands, kernel, 1, NULL, &global, &local, 0, NULL, NULL);
+    err = clEnqueueNDRangeKernel(commands, round1_kernel, 1, NULL, &global, &local, 0, NULL, NULL);
     if (err)
     {
-        printf("Error: Failed to execute kernel!\n");
+        printf("Error: Failed to execute round1_kernel!\n");
         printf("Global: %ld, local: %ld", global, local);
         return EXIT_FAILURE;
     }
@@ -206,21 +249,78 @@ int main(int argc, char **argv)
     }
 
     // Validate our results
-    //
-    correct = 0;
-    printf("results: ");
+    printf("Round 1 results\n============================\n");
     for (int i = 0; i < candidates; i++)
     {
-        printf("%d ", results[i]);
+        percent_votes[i] = results[i] / (double)voters;
+        printf("Candidate [%d] got %d/%d which is %0.2lf%%\n", i + 1, results[i], voters, percent_votes[i] * 100);
+        if (percent_votes[i] > 0.5)
+            winner = i + 1;
     }
-    printf("\n");
+    if (winner)
+        printf("%d 1\n", winner);
+    else
+    {
+        // Round 2
+        int top2[2];
+        int round2_sum[2];
+        getTop2(results, candidates, top2);
+        round2_kernel = clCreateKernel(program, "round2_k", &err);
+        if (!round2_kernel || err != CL_SUCCESS)
+        {
+            printf("Error: Failed to create compute round1_kernel!\n");
+            exit(1);
+        }
+        err = 0;
+        err = clSetKernelArg(round2_kernel, 0, sizeof(cl_mem), &d_vote_pref);
+        err |= clSetKernelArg(round2_kernel, 1, sizeof(cl_mem), &d_top2_sum);
+        err |= clSetKernelArg(round2_kernel, 2, sizeof(cl_mem), NULL);
+        err |= clSetKernelArg(round2_kernel, 3, sizeof(int), &candidates);
+        err |= clSetKernelArg(round2_kernel, 4, sizeof(int), &voters);
+        err |= clSetKernelArg(round2_kernel, 5, sizeof(int), &top2[0]);
+        err |= clSetKernelArg(round2_kernel, 6, sizeof(int), &top2[1]);
+        if (err != CL_SUCCESS)
+        {
+            printf("Error: Failed to set round1_kernel arguments! %d\n", err);
+            exit(1);
+        }
+        err = clEnqueueNDRangeKernel(commands, round2_kernel, 1, NULL, &global, &local, 0, NULL, NULL);
+        if (err)
+        {
+            printf("Error: Failed to execute round2_kernel!\n");
+            printf("Global: %ld, local: %ld", global, local);
+            return EXIT_FAILURE;
+        }
+
+        // Wait for the command commands to get serviced before reading back results
+        //
+        clFinish(commands);
+
+        // Read back the results from the device to verify the output
+        //
+        err = clEnqueueReadBuffer(commands, d_top2_sum, CL_TRUE, 0, sizeof(int) * 2, round2_sum, 0, NULL, NULL);
+        end = clock();
+        cpu_time_used = ((double)(end - start)) / CLOCKS_PER_SEC;
+        if (err != CL_SUCCESS)
+        {
+            printf("Error: Failed to read output array! %d\n", err);
+            exit(1);
+        }
+        printf("Round 2 results\n============================\n");
+        int arg_winner = (round2_sum[0] > round2_sum[1]) ? 0 : 1;
+        printf("Candidate [%d] got %d/%d which is %0.2lf%%\n", top2[0] + 1, round2_sum[0], voters, (round2_sum[0] / (double)voters) * 100);
+        printf("Candidate [%d] got %d/%d which is %0.2lf%%\n", top2[1] + 1, round2_sum[1], voters, (round2_sum[1] / (double)voters) * 100);
+        winner = top2[arg_winner] + 1;
+        printf("%d 2\n", winner);
+        printf("Total time taken %f seconds\n", cpu_time_used);
+    }
 
     // Shutdown and cleanup
     //
     clReleaseMemObject(d_vote_pref);
     clReleaseMemObject(output);
     clReleaseProgram(program);
-    clReleaseKernel(kernel);
+    clReleaseKernel(round1_kernel);
     clReleaseCommandQueue(commands);
     clReleaseContext(context);
 
--- red_kernel.cl
+++ red_kernel.cl
@@ -1,13 +1,41 @@
-kernel void vote_sum(__global int *vote_pref, __global int *vote_summary,
-                     const uint candidates, const uint voters) {
+kernel void round1_k(__global int *vote_pref, __global int *vote_summary,
+                     __local int *local_vote_sum, const int candidates,
+                     const int voters) {
   int idx = get_global_id(0);
   if (idx < voters) {
-    // int local_size = get_local_size(0);
-    // int global_size = get_global_size(0);
-    // int groupId = get_group_id(0);
     int cand_choice = vote_pref[idx * candidates];
     for (int i = 0; i < candidates; i++) {
-      vote_summary[i] = work_group_reduce_add((i + 1) == cand_choice ? 1 : 0);
+      local_vote_sum[i] = work_group_reduce_add((i + 1) == cand_choice ? 1 : 0);
+    }
+    if (idx == get_group_id(0) * get_local_size(0)) {
+      for (int i = 0; i < candidates; i++)
+        atomic_add(&vote_summary[i], local_vote_sum[i]);
+    }
+  }
+}
+
+kernel void round2_k(__global int *vote_pref, __global int *top2_sum,
+                     __local int *local_top2_sum, const int candidates,
+                     const int voters, const int cand_1, const int cand_2) {
+  int idx = get_global_id(0);
+  int tmp_choice;
+  int cand_choice = 0;
+  if (idx < voters) {
+
+    for (int i = 0; i < candidates; i++) {
+      tmp_choice = vote_pref[idx * candidates + i] - 1;
+      if (tmp_choice == cand_1 || tmp_choice == cand_2){
+        cand_choice = tmp_choice;
+        break;
+      }
+    }
+
+    local_top2_sum[0] = work_group_reduce_add( cand_1 == cand_choice? 1 : 0);
+    local_top2_sum[1] = work_group_reduce_add( cand_2 == cand_choice? 1 : 0);
+
+    if (idx == get_group_id(0) * get_local_size(0)) {
+        atomic_add(&top2_sum[0], local_top2_sum[0]);
+        atomic_add(&top2_sum[1], local_top2_sum[1]);
     }
   }
 }
